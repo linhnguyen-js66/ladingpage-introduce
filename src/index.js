f351d6de174784dd500b42d35aa6cd23ea25fb9e
linhnguyen-js66 <linhnguyenchi227@gmail.com>
2021-09-06 16:25:29 +0700

    add babel to project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const introduceMe = {
+    age:21,
+    job:"student",
+    address:"68 Xuan Thuy, Cau Giay, Ha Noi"
+}
+const introducePage = {
+    page:"introduce about NHL",
+    localHost:'http://localhost:8080/'
+}
+console.log(introducePage)
+console.log(introduceMe)
