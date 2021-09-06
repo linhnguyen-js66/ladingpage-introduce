8c7fd9283dc855637a7c5f8beca0beda594c1d85
linhnguyen-js66 <linhnguyenchi227@gmail.com>
2021-09-07 04:09:16 +0700

    fix size image

--- src/screens/01-HomePage-Screen/01-HomePage-Screen.jsx
+++ src/screens/01-HomePage-Screen/01-HomePage-Screen.jsx
@@ -3,7 +3,7 @@ const HomePageScreen01 = () => {
     return (
         <section className="home-main">
             <section className="contain-image-personal">
-                <img src={require('../../assets/linhbeo.jpg')} className="image-personal" />
+                <img src="https://i.ibb.co/16MHtDD/IMG-6768-1.jpg" className="image-personal" />
                 <div className="background-under-img"></div>
 
             </section>
